fb011c91a130507dd8548b714af3883b1b4831cd
up shell <40010260+anggi135@users.noreply.github.com>
2023-12-12 11:16:06 +0700

    Create new-tugas10.py

---
+++ new-tugas10.py
@@ -0,0 +1,14 @@
+# Perulangan for
+print('perulangan for')
+print('==============')
+for g in range(9):
+    print( g )
+print('')
+print('')
+# Perulangan for bertingkat
+print('perulangan bertingak')
+print('====================')
+for gg in range(3):
+    for ti in range(2):
+        print(gg, ti)
+    
